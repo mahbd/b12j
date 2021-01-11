e311f4ee48917e5f732f5894ff1afa9039cc72ca
Mahmudul Alam <mahmudula2000@gmail.com>
2021-01-11 09:28:21 +0600

    Remove details from submissions

--- api/serializers.py
+++ api/serializers.py
@@ -95,7 +95,7 @@ class SubmissionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submission
         fields = '__all__'
-        read_only_fields = ['verdict', 'contest', 'by', 'time_code', 'details']
+        read_only_fields = ['verdict', 'contest', 'by', 'time_code']
         write_only_fields = ['code']
 
     def create(self, validated_data):
