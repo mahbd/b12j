4a5fe68a62290c6247e8d9578c262e54e864af8b
Mahmudul Alam <mahmudula2000@gmail.com>
2021-05-11 20:28:41 +0600

    run build

--- b12j-front/src/apps/submission/submissionList.jsx
+++ b12j-front/src/apps/submission/submissionList.jsx
@@ -14,16 +14,18 @@ const SubmissionList = ({match}) => {
             <table className="table table-bordered table-striped">
                 <thead>
                 <tr>
+                    <th className={"bg-dark rounded-3 text-white"}>Id</th>
+                    <th className={"bg-dark rounded-3 text-white"}>By</th>
                     <th className={"bg-dark rounded-3 text-white"}>Problem</th>
                     <th className={"bg-dark rounded-3 text-white"}>Verdict</th>
-                    <th className={"bg-dark rounded-3 text-white"}>By</th>
                 </tr>
                 </thead>
                 <tbody>
                 {submissionList.map((submission) => <tr key={submission.id}>
-                    <td><Link to={`/submissions/${submission.id}`}>{submission.problem_title}</Link></td>
-                    <td>{submission.verdict}</td>
-                    <td>{userActs.firstName(submission.by)}</td></tr>)}
+                    <td><Link to={`/submissions/${submission.id}`}>{submission.id}</Link></td>
+                    <td>{userActs.fullName(submission.by)}</td>
+                    <td><Link to={`/problems/${submission.problem}`}>{submission.problem_title}</Link></td>
+                    <td>{submission.verdict}</td></tr>)}
                 </tbody>
             </table>
             {pagination('/submissions/page=', pages, page)}
