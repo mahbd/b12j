163788e7ba69b8a4b67b6043459010c4c28481b7
Mahmudul Alam <mahmudula2000@gmail.com>
2021-10-18 07:55:55 +0600

    Take email address from environment variable

--- b12j/settings.py
+++ b12j/settings.py
@@ -137,7 +137,7 @@ LOGIN_URL = '/users/login/'  #
 #####################################################################
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  #
 EMAIL_HOST = "smtp.gmail.com"  #
-EMAIL_HOST_USER = 'mahmuduly2000@gmail.com'  #
+EMAIL_HOST_USER = os.environ.get('EMAIL')  #
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')  #
 EMAIL_PORT = 587  #
 EMAIL_USE_TLS = True  #
