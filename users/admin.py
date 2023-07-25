df9b114ce6b60cff38a61a746d6e48805227036f
Mahmudul Alam <mahmudula2000@gmail.com>
2023-07-25 09:33:18 +0600

    added comma to admin user search field

--- users/admin.py
+++ users/admin.py
@@ -4,5 +4,5 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    search_fields = ['username', 'first_name' 'last_name', 'email']
+    search_fields = ['username', 'first_name', 'last_name', 'email']
     list_display = ['username', 'first_name', 'email']
